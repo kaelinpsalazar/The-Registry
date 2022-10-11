d85d14924524de65896ee7948334f544a95926d1
dylangriess <dylangriess@gmail.com>
2022-10-11 14:24:44 -0600

    added Gifter model

---
+++ server/models/Gifter.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const gifterSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must provide a valid email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  message: {
+    type: String,
+    minLength: 12,
+  },
+});
+
+// set up pre-save middleware to create password
+gifterSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+gifterSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const Gifter = model("Gifter", gifterSchema);
+
+module.exports = Gifter;
