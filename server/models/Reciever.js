f4344d94ea9491e3cb895f1c56e177799f53b97e
dylangriess <dylangriess@gmail.com>
2022-10-11 20:01:07 -0600

    changed receiver to recipient

--- server/models/Reciever.js
+++ server/models/Reciever.js
@@ -1,7 +1,7 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const recieverSchema = new Schema({
+const recipientSchema = new Schema({
   name: {
     name: String,
     required: true,
@@ -12,19 +12,18 @@ const recieverSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
+    match: [/.+@.+\..+/, "Must match an email address!"],
   },
   password: {
     type: String,
     required: true,
     minlength: 12,
   },
-  
 });
 
 // set up pre-save middleware to create password
-profileSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
+recipientSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
   }
@@ -33,10 +32,10 @@ profileSchema.pre('save', async function (next) {
 });
 
 // compare the incoming password with the hashed password
-profileSchema.methods.isCorrectPassword = async function (password) {
+recipientSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const Reciever = model('Reciever', recieverSchema);
+const Recipient = model("Recipient", recipientSchema);
 
-module.exports = Reciever;
+module.exports = Recipient;
