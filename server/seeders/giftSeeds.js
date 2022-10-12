60f414898c6aa7df63c82d01a1dfac7e4b6d2b34
SLeBron <sophieclebron@gmail.com>
2022-10-11 20:46:33 -0600

    added seed data

---
+++ server/seeders/giftSeeds.js
@@ -0,0 +1,34 @@
+[
+    {
+        "product": "KitchenAid Mixer Artisan Tilt-Head Stand 5-qt. Mixer",
+        "price": 350,
+        "store": "Bed Bath & Beyond",
+        "description": "",
+        "url": "https://www.bedbathandbeyond.com/store/product/kitchenaid-mixer-artisan-tilt-head-stand-5-qt-mixer/102986",
+        "imageUrl": "https://m.media-amazon.com/images/I/71DegSVExAL.jpg"
+    },
+    {
+        "product": "",
+        "price": "",
+        "store": "",
+        "description": "",
+        "url": "",
+        "imageUrl": ""
+    },
+    {
+        "product": "",
+        "price": "",
+        "store": "",
+        "description": "",
+        "url": "",
+        "imageUrl": ""
+    },
+    {
+        "product": "",
+        "price": "",
+        "store": "",
+        "description": "",
+        "url": "",
+        "imageUrl": ""
+    }
+]
