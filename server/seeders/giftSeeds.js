a1a6726ee035168bd15ba836c558c704d6e79f4f
SLeBron <sophieclebron@gmail.com>
2022-10-12 11:24:15 -0600

    added seed data

--- server/seeders/giftSeeds.js
+++ server/seeders/giftSeeds.js
@@ -8,20 +8,20 @@
         "imageUrl": "https://m.media-amazon.com/images/I/71DegSVExAL.jpg"
     },
     {
-        "product": "",
-        "price": "",
-        "store": "",
+        "product": "Raffael Carved Wood Bar Cabinet with Storage",
+        "price": 1500,
+        "store": "Crate&Barrel",
         "description": "",
-        "url": "",
-        "imageUrl": ""
+        "url": "https://www.crateandbarrel.com/raffael-carved-wood-bar-cabinet-with-storage/s336174",
+        "imageUrl": "https://cb.scene7.com/is/image/Crate/RaffaelCrvdWoodBarCbntSOSSS20_1x1/$web_pdp_main_carousel_zoom_med$/211014091251/raffael-carved-wood-bar-cabinet.jpg"
     },
     {
-        "product": "",
-        "price": "",
-        "store": "",
+        "product": "Mason Modern Melamine Serving Bowl",
+        "price": 29,
+        "store": "Pottery Barn",
         "description": "",
-        "url": "",
-        "imageUrl": ""
+        "url": "https://www.potterybarn.com/products/mason-modern-melamine-serving-bowl/?pkey=call-serveware",
+        "imageUrl": "https://assets.pbimgs.com/pbimgs/ab/images/dp/wcm/202238/0124/mason-modern-melamine-serving-bowl-z.jpg"
     },
     {
         "product": "",
