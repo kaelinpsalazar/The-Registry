36f55b8a7ff32b3a4bc0c7e2812f46eea563986f
dylangriess <dylangriess@gmail.com>
2022-10-12 11:37:39 -0600

    adding queries/mutations to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,6 +6,12 @@ const typeDefs = gql`
     name: String
     email: String
   }
+  type Message {
+    _id: ID
+    messageText: String
+    messageAuthor: String
+    createdAt: String
+  }
   type Recipient {
     _id: ID
     name: String
@@ -18,6 +24,27 @@ const typeDefs = gql`
     price: Number
     store: String
     description: String
+  }
+
+  type Query {
+    gifters: [Gifter]
+    gifter(name: String!): Gifter
+    messages(name: String): [Message]
+    message(messageId: ID!): Message
+  }
+
+  type Mutation {
+    addGift(
+      product: String!
+      price: Number!
+      store: String!
+      description: String!
+      url: String!
+    ): Gift
+    removeGift(giftId: ID!): Gift
+    addMessage(messageText: String!): Message
+    removeMessage(messageId: ID!): Message
+    login(email: String!, password: String!): Auth
   }
 `;
 
