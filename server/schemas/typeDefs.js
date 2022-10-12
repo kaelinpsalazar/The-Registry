b432afd68b7925c61ca3c5469ec6484f2912f5ca
dylangriess <dylangriess@gmail.com>
2022-10-11 19:36:49 -0600

    added typeDefs based on models

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,24 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql;
+const typeDefs = gql`
+  type Gifter {
+    _id: ID
+    name: String
+    email: String
+  }
+  type Recipient {
+    _id: ID
+    name: String
+    email: String
+    password: String
+  }
+  type Gift {
+    _id: ID
+    product: String
+    price: Number
+    store: String
+    description: String
+  }
+`;
 
 module.exports = typeDefs;
