08cea194e2d4475c002f5afb4072b6a4980fae53
Ghost <mbflug@gmail.com>
2022-10-13 15:08:30 -0600

    please review this

---
+++ server/models/Gifter.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const gifterSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must provide a valid email address!"],
+  },
+  message: {
+    type: Schema.Types.ObjectID,
+    ref: 'Message',
+  },
+});
+
+
+const Gifter = model("Gifter", gifterSchema);
+
+module.exports = Gifter;
---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const Gifter = require("./Gifter");
+const Recipient = require("./Recipient");
+const Gift = require("./Gift");
+const Registry = require("./Registry");
+const Message = require("./Message");
+
+module.exports = { Gifter, Recipient, Gift, Registry, Message };
