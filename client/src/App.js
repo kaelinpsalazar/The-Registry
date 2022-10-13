05b7a1b0d06782e58d40bac2061b108f848e6869
Al Wong <timberhead1121@gmail.com>
2022-10-12 18:55:03 -0600

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Wedding from "./pages/Wedding";
+import Graduation from "./pages/Graduation";
+import Newbaby from "./pages/Newbaby";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Receiverlogin from "./components/helpers/Receiverlogin";
+import Searchregistry from "./components/helpers/Searchregistry";
+import Retaillinks from "./components/helpers/Retaillinks";
+import Givemoney from "./components/helpers/Givemoney";
+import { Router } from '@material-ui/icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Receiverlogin />} />
+          <Route path="/" element={<Searchregistry />} />
+          <Route path="/pages" element={<Wedding />} />
+          <Route path="/pages" element={<Graduation />} />
+          <Route path="/pages" element={<Newbaby />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
