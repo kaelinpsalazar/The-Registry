0536fb6449c48c9d7925dd1cf384e8cf50fa82a5
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-10-13 18:17:12 -0600

    adjusted the App.js file to remove stripe bug.

--- client/src/App.js
+++ client/src/App.js
@@ -7,61 +7,6 @@ import Newbaby from "./pages/Newbaby";
 import Header from "./components/helpers/Header";
 import Footer from "./components/helpers/Footer";
 
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "./CheckoutForm";
-
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
-
-const successMessage = () => {
-  return <div className="success-msg">... ...</div>;
-};
-
-const cart = () => {
-  return <React.Fragment>... ...</React.Fragment>;
-};
-
-function App() {
-  const [paymentCompleted, setPaymentCompleted] = useState(false);
-
-  return (
-    <div className="container">
-      <div className="py-5 text-center">
-        <h4>
-          Payment form-{" "}
-          <a
-            href="https://www.cluemediator.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            The Registry
-          </a>
-        </h4>
-      </div>
-
-      <div className="row s-box">
-        {paymentCompleted ? (
-          successMessage()
-        ) : (
-          <React.Fragment>
-            <div className="col-md-5 order-md-2 mb-4">{cart()}</div>
-            <div className="col-md-7 order-md-1">
-              <Elements stripe={stripePromise}>
-                <CheckoutForm
-                  amount={2000}
-                  setPaymentCompleted={setPaymentCompleted}
-                />
-              </Elements>
-            </div>
-          </React.Fragment>
-        )}
-      </div>
-    </div>
-  );
-}
-
 // import Receiverlogin from "./components/helpers/Receiverlogin";
 // import Searchregistry from "./components/helpers/Searchregistry";
 // import Retaillinks from "./components/helpers/Retaillinks";
