b3896d1ecd23dd577a9be104275f4d23158356ba
Ghost <mbflug@gmail.com>
2022-10-14 16:03:16 -0600

    updating home page

--- client/src/components/helpers/Header.js
+++ client/src/components/helpers/Header.js
@@ -1,53 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
 
-// import "../pages/styles/Header.css";
+import Nav from "react-bootstrap/Nav";
 
-// import Auth from '../../utils/auth';
-
-
-const Header = () => {
-  const logout = (event) => {
-    event.preventDefault();
-    // Auth.logout();
-  };
-
+function Header() {
   return (
-    <div>
-      <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <div>
-          <Link className="text-light" to="/">
-            <h1 className="m-0">the Registry.com</h1>
-          </Link>
-          <p className="m-0">Welcome to the Registry.</p>
-        </div>
-        <div>
-          {/* {Auth.loggedIn() ? (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
-                {Auth.getProfile().data.username}'s profile
-              </Link>
-              <button className="btn btn-lg btn-light m-2" onClick={logout}>
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link className="btn btn-lg btn-info m-2" to="/login">
-                Login
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/signup">
-                Signup
-              </Link>
-            </>
-          )} */}
-          </div>
-        </div>
-      </header>
-    </div>
+    <>
+      <Nav className="justify-content-end" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Login</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Sign Up</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
   );
 }
 
