f40384c3fec05a6e65c491c83b4554aa6e370100
dylangriess <dylangriess@gmail.com>
2022-10-15 21:12:21 -0600

    styling changes to all pages

--- client/src/pages/Graduation.js
+++ client/src/pages/Graduation.js
@@ -8,7 +8,7 @@ import MessageList from "../components/helpers/MessageList";
 import StripeContainer from "../components/helpers/StripeContainer";
 import { useState } from "react";
 
-import "./styles/wedding.css";
+import "./styles/graduation.css";
 
 function Graduation() {
   const [showItem, setShowItem] = useState(false);
@@ -19,46 +19,48 @@ function Graduation() {
 
   return (
     <div>
-      <div className="retaillinks">
-        <h1>Retail Stores We're Registered At:</h1>
-        <button className="btn btn-primary"></button>
-        <button className="btn btn-primary"></button>
-        <button className="btn btn-primary"></button>
-      </div>
-      <div className="wedding">
-        <h1>Our Wish List Items</h1>
-        <div className="wishListList">
-          <WishListItems />
+      <div className="graduationBg">
+        <div className="retaillinks">
+          <h1>Retail Stores We're Registered At:</h1>
+          <button className="btn btn-primary"></button>
+          <button className="btn btn-primary"></button>
+          <button className="btn btn-primary"></button>
         </div>
-
-        <div className="messageBox">
-          <div
-            className="col-4 col-md-4 mb-3 p-1"
-            style={{ border: "1px solid #1a1a1a" }}
-          >
-            <GifterInput />
+        <div className="wedding">
+          <h1>Our Wish List Items</h1>
+          <div className="wishListList">
+            <WishListItems />
           </div>
-          <div className="message col-12 col-md-10 mb-3">
-            <MessageList
-              messages={messages}
-              title="Messages for the Graduate"
-            />
+
+          <div className="messageBox">
+            <div
+              className="col-4 col-md-4 mb-3 p-1"
+              style={{ border: "1px solid #1a1a1a" }}
+            >
+              <GifterInput />
+            </div>
+            <div className="message col-12 col-md-10 mb-3">
+              <MessageList
+                messages={messages}
+                title="Messages for the Graduate"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="givemoney">
-        <h1>...or you can just give us money!</h1>
-        {showItem ? (
-          <StripeContainer />
-        ) : (
-          <>
-            <button onClick={() => setShowItem(true)}>
-              Donate $100 to The Registry!
-            </button>
-          </>
-        )}
+        <div className="givemoney">
+          <h1>...or you can just give us money!</h1>
+          {showItem ? (
+            <StripeContainer />
+          ) : (
+            <>
+              <button onClick={() => setShowItem(true)}>
+                Donate $100 to The Registry!
+              </button>
+            </>
+          )}
 
-        <h3>...or you can just give money</h3>
+          <h3>...or you can just give money</h3>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/Newbaby.js
+++ client/src/pages/Newbaby.js
@@ -9,7 +9,7 @@ import MessageList from "../components/helpers/MessageList";
 import StripeContainer from "../components/helpers/StripeContainer";
 import { useState } from "react";
 
-import "./styles/wedding.css";
+import "./styles/newbaby.css";
 
 function Newbaby() {
   const [showItem, setShowItem] = useState(false);
@@ -23,40 +23,42 @@ function Newbaby() {
 
   return (
     <div>
-      <div className="retaillinks">
-        <h1>Retail Stores We're Registered At:</h1>
-        <button className="btn btn-primary"></button>
-        <button className="btn btn-primary"></button>
-        <button className="btn btn-primary"></button>
-      </div>
-      <div className="wedding">
-        <h1>Our Wish List Items</h1>
-        <div className="wishListList">
-          <WishListItems />
+      <div className="newbabyBg">
+        <div className="retaillinks">
+          <h1>Retail Stores We're Registered At:</h1>
+          <button className="btn btn-primary"></button>
+          <button className="btn btn-primary"></button>
+          <button className="btn btn-primary"></button>
         </div>
-        <div className="messageBox">
-          <div
-            className="col-4 col-md-4 mb-3 p-1"
-            style={{ border: "1px solid #1a1a1a" }}
-          >
-            <GifterInput />
+        <div className="wedding">
+          <h1>Our Wish List Items</h1>
+          <div className="wishListList">
+            <WishListItems />
           </div>
-          <div className="message col-12 col-md-10 mb-3">
-            <MessageList title="Messages for the new parents" />
+          <div className="messageBox">
+            <div
+              className="col-4 col-md-4 mb-3 p-1"
+              style={{ border: "1px solid #1a1a1a" }}
+            >
+              <GifterInput />
+            </div>
+            <div className="message col-12 col-md-10 mb-3">
+              <MessageList title="Messages for the new parents" />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="givemoney">
-        <h1>...or you can just give us money!</h1>
-        {showItem ? (
-          <StripeContainer />
-        ) : (
-          <>
-            <button onClick={() => setShowItem(true)}>
-              Donate $100 to The Registry!
-            </button>
-          </>
-        )}
+        <div className="givemoney">
+          <h1>...or you can just give us money!</h1>
+          {showItem ? (
+            <StripeContainer />
+          ) : (
+            <>
+              <button onClick={() => setShowItem(true)}>
+                Donate $100 to The Registry!
+              </button>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
