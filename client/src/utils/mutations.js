7aaf82e099a1e614eacb7040d568340b695c4f3f
Ghost <mbflug@gmail.com>
2022-10-16 22:36:37 -0600

    updated signup

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,12 +24,12 @@ export const LOGIN_RECIPIENT = gql`
 `;
 
 export const ADD_REGISTER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
       token
       user {
         _id
-        username
+        name
       }
     }
   }
