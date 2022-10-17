9886b19e7a1d1c8c22466694670c1c923ecbcc42
Ghost <mbflug@gmail.com>
2022-10-16 22:17:07 -0600

    pushing register

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Navigate, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import "./styles/profile.css";
@@ -45,27 +46,65 @@ const Signup = () => {
   return (
     <div>
       <div className="container flex-row justify-center">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Name</Form.Label>
-            <Form.Control placeholder="Enter name" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+        {data ? (
+          <p>
+            Success! You may now head{" "}
+            <Link to="./profile">You may head to your profile!</Link>
+          </p>
+        ) : (
+          <Form onSubmit={handleFormSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label
+                className="form-input"
+                placeholder="Your username"
+                name="username"
+                type="text"
+                value={formState.name}
+                onChange={handleChange}
+              >
+                Name
+              </Form.Label>
+              <Form.Control placeholder="Enter name" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                className="form-input"
+                placeholder="Enter email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Button className="login-btn" variant="!important" type="submit">
-            Submit
-          </Button>
-        </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            <Button
+              className="login-btn"
+              style={{ cursor: "pointer" }}
+              variant="!important"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form>
+        )}
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+        )}
       </div>
     </div>
   );
