14db6c3285356c5e13233bd8d50c331886d781be
dylangriess <107587452+dylangriess@users.noreply.github.com>
2022-10-16 22:38:43 -0600

    Update Wedding.js

--- client/src/pages/Wedding.js
+++ client/src/pages/Wedding.js
@@ -4,18 +4,12 @@ import { Elements } from "@stripe/react-stripe-js";
 import imageK from "../assets/images/imageK.jpg";
 
 import WishListItems from "../components/helpers/WishListItems";
-import retaillinks from "../components/helpers/Retaillinks";
 import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { QUERY_GIFTS, QUERY_MESSAGES } from "../utils/queries";
 import GifterInput from "../components/helpers/GifterInput";
 import MessageList from "../components/helpers/MessageList";
 import StripeContainer from "../components/helpers/StripeContainer";
-
-// import { useState } from "react";
-
-
-
 import "./styles/wedding.css";
 import Container from "react-bootstrap/esm/Container";
 
@@ -35,31 +29,6 @@ function Wedding() {
   return (
     <div>
       <div className="weddingBg">
-
-        <Container className="imagecontainerW" style={{ backgroundImage:`url(${imageK})`}}>
-            <h1 className ="pictureTitleW">The Wedding Registry</h1>
-        </Container>
-        <div className="retaillinks p-5 justify-content-around d-flex">
-          <h1>Retail Stores We're Registered At:</h1>
-          <a href="https://www.bestbuy.com/" target="_blank">
-            <button className="btn btn-1">Best Buy</button>
-          </a>
-          <a href="https://www.target.com/" target="_blank">
-            <button className="btn btn-1">Target</button>
-          </a>
-          <a href="https://www.bedbathandbeyond.com/" target="_blank">
-            <button className="btn btn-1">Bed Bath & Beyond</button>
-          </a>
-        </div>
-{/*
-        <div className="retaillinks">
-          <h1>Retail Stores We're Registered At:</h1>
-          <button className="btn btn-primary"></button>
-          <button className="btn btn-primary"></button>
-          <button className="btn btn-primary"></button>
-        </div> */}
-        <img src={imageK} width="700" alt="wedding" />
-
         <Container
           className="imagecontainerW"
           style={{ backgroundImage: `url(${imageK})` }}
@@ -67,12 +36,12 @@ function Wedding() {
           <h1 className="pictureTitleW">The Wedding Registry</h1>
         </Container>
 
-
         <div className="wedding">
           <h1>Our Wish List Items</h1>
           <div className="wishListList">
             <WishListItems />
           </div>
+
           <div className="retaillinks p-5 justify-content-around d-flex">
             <h1>Retail Stores We're Registered At:</h1>
             <a href="https://www.crateandbarrel.com/" target="_blank">
@@ -85,21 +54,6 @@ function Wedding() {
               <button className="btn btn-1">Bed Bath & Beyond</button>
             </a>
           </div>
-
-          <div
-            className="col-4 col-md-4 mb-3 p-1"
-            style={{ border: "1px solid #1A1A1A" }}
-          >
-            <GifterInput />
-          </div>
-          <div className="col-12 col-md-10 mb-3">
-            <MessageList title="Messages for the Newlyweds" />
-          </div>
-
-
-
-
-
 
           <div className="messageBox d-flex">
             <div className="col-6 col-md-6 mb-3">
@@ -119,7 +73,10 @@ function Wedding() {
             <StripeContainer />
           ) : (
             <>
-              <button onClick={() => setShowItem(true)}>
+              <button
+                className="moneybtn btn btn-primary justify-content-center m-3"
+                onClick={() => setShowItem(true)}
+              >
                 Donate $100 to The Registry!
               </button>
             </>
