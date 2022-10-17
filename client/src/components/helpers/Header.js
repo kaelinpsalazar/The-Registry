8709b90bd9f688f44f592a8ed964b260ddf0b4ca
dylangriess <dylangriess@gmail.com>
2022-10-17 13:04:06 -0600

    changes to header, deleted navbar.js

--- client/src/components/helpers/Header.js
+++ client/src/components/helpers/Header.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 // import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 // import Modal from "react-bootstrap/Modal";
 import Nav from "react-bootstrap/Nav";
 import "./styles/header.css";
@@ -13,22 +14,28 @@ function BrandExample() {
         <div className="nav-container">
           <div>
             <Navbar.Brand className="text" href="#home">
-              <Nav.Link className="theRegistry" href="/">
-                The Registry
-              </Nav.Link>
+              <Link to="/">
+                <Button className="theRegistry">The Registry</Button>
+              </Link>
             </Navbar.Brand>
           </div>
           <div>
             <Container className="buttons">
-              <Button className="button-1" variant="primary" href="/login">
-                Login
-              </Button>
-              <Button className="button-2" variant="primary" href="/signup">
-                Register
-              </Button>
-              <Button className="button-3" variant="primary" href="/profile">
-                Profile
-              </Button>
+              <Link to="/login">
+                <Button className="button-1" variant="primary">
+                  Login
+                </Button>
+              </Link>
+              <Link to="/signup">
+                <Button className="button-2" variant="primary">
+                  Register
+                </Button>
+              </Link>
+              <Link to="/profile">
+                <Button className="button-3" variant="primary">
+                  Profile
+                </Button>
+              </Link>
             </Container>
           </div>
         </div>
